571d8798835769fc026fbfc3a7ea0127dc7585a1
oksanaivashko <oksivashko13@gmail.com>
2023-02-08 17:34:26 -0600

    dumb mistakes

--- launch_conf.tf
+++ launch_conf.tf
@@ -2,4 +2,6 @@ resource "aws_launch_configuration" "linux" {
   name = "task-launch-config"
   image_id = "ami-06e85d4c3149db26a"
   instance_type = "t2.micro"
+  security_groups = [ aws_security_group.lc_sg.id ]
+  key_name = "windows-key"
 }
