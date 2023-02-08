e187784e9495612a100ab3fab9fa4080e51302b2
oksanaivashko <oksivashko13@gmail.com>
2023-02-08 17:33:53 -0600

    changes

--- launch_conf.tf
+++ launch_conf.tf
@@ -1,4 +1,4 @@
-data "aws_launch_configuration" "linux" {
+resource "aws_launch_configuration" "linux" {
   name = "task-launch-config"
   image_id = "ami-06e85d4c3149db26a"
   instance_type = "t2.micro"
