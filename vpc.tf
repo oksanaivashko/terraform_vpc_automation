9aee4f193dda882a4e5abc12fe6e3067b87f16de
oksanaivashko <oksivashko13@gmail.com>
2023-02-08 14:35:06 -0600

    added vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,36 @@
-resource "aws_vpc" "task_vpc" {       
+resource "aws_vpc" "tf_vpc" {       
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 }
+
+resource "aws_subnet" "public_a" {
+    vpc_id = aws_vpc.tf_vpc.id
+  availability_zone = "us-west-2a"
+  cidr_block       = "10.0.1.0/24"
+}
+resource "aws_subnet" "public_b" {
+    vpc_id = aws_vpc.tf_vpc.id
+  availability_zone = "us-west-2a"
+  cidr_block       = "10.0.1.0/24"
+}
+resource "aws_subnet" "public_c" {
+    vpc_id = aws_vpc.tf_vpc.id
+  availability_zone = "us-west-2a"
+  cidr_block       = "10.0.1.0/24"
+}
+resource "aws_internet_gateway" "int_gway" {
+  vpc_id = aws_vpc.tf_vpc.id
+
+  tags = {
+    Name = "int_gway"
+  }
+}
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.tf_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.int_gway.id
+  }
+}
+
