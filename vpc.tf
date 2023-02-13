79280d6286c67b7f572da6eca8530d353731eb9e
oksanaivashko <oksivashko13@gmail.com>
2023-02-12 22:32:43 -0600

    associate subnets with route table

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 #Create VPC
-resource "aws_vpc" "tf_vpc" {       
+resource "aws_vpc" "my_vpc" {       
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
    tags = {
@@ -7,80 +7,154 @@ resource "aws_vpc" "tf_vpc" {
     }
 }
 
-#Create public subnets in three az
+
+
+#   --- Create public subnets in three az ---
+
+
 resource "aws_subnet" "public_a" {
-    vpc_id = aws_vpc.tf_vpc.id
+    vpc_id = aws_vpc.my_vpc.id
   availability_zone = "us-west-2a"
   cidr_block       = "10.0.1.0/24"
+  tags = {
+        Name = "public_subnet_a"
+  }
 }
 resource "aws_subnet" "public_b" {
-    vpc_id = aws_vpc.tf_vpc.id
+    vpc_id = aws_vpc.my_vpc.id
   availability_zone = "us-west-2a"
   cidr_block       = "10.0.2.0/24"
+  tags = {
+        Name = "public_subnet_b"
+  }
 }
 resource "aws_subnet" "public_c" {
-    vpc_id = aws_vpc.tf_vpc.id
+    vpc_id = aws_vpc.my_vpc.id
   availability_zone = "us-west-2a"
   cidr_block       = "10.0.3.0/24"
+  tags = {
+        Name = "public_subnet_c"
+  }
 }
 
-#Create IntGateway and connect to the VPC
+
+#   ---Create a public route table ---
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+ route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.int_gway.id
+  }
+}
+
+#  ---Associate subnets to Public Route Table---
+
+resource "aws_route_table_association" "public_route_table1" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+resource "aws_route_table_association" "public_route_table2" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+resource "aws_route_table_association" "public_route_table3" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+
+
+#  ---- Create IntGateway and connect to the VPC ---   
+
 resource "aws_internet_gateway" "int_gway" {
-  vpc_id = aws_vpc.tf_vpc.id
+  vpc_id = aws_vpc.my_vpc.id
 
   tags = {
     Name = "int_gway"
   }
 }
 
-#Create public route table and connect to IntGateway
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.tf_vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.int_gway.id
-  }
-}
+#    ----Allocate Elastic IP Address------
 
-#Create private subnets in three az
-resource "aws_subnet" "private_subnet_a" {
-    vpc_id = aws_vpc.tf_vpc.id
-  availability_zone = "us-west-2a"
-  cidr_block       = "10.0.11.0/24"
-}
-resource "aws_subnet" "private_subnet_b" {
-    vpc_id = aws_vpc.tf_vpc.id
-  availability_zone = "us-west-2a"
-  cidr_block       = "10.0.12.0/24"
-}
-resource "aws_subnet" "private_subnet_c" {
-    vpc_id = aws_vpc.tf_vpc.id
-  availability_zone = "us-west-2a"
-  cidr_block       = "10.0.13.0/24"
-
-}
-#Create NatGateway with the public subnet and connect to private subnet
-resource "aws_nat_gateway" "nat_gway" {
-  connectivity_type = "public"
-  subnet_id         = aws_subnet.public_a.id
-  allocation_id = aws_eip.nat_eip.id
-}
-
-#Allocate Elastic IP Address
-resource "aws_eip""nat_eip"{
+resource "aws_eip" "nat_gateway_eip"{
     vpc = true
     tags = {
         Name = "eip"
     }
 }
 
-#Create private route table and attach to VPC
+
+#  ---- Create private subnets in three az ---
+resource "aws_subnet" "private_subnet_a" {
+    vpc_id = aws_vpc.my_vpc.id
+  availability_zone = "us-west-2a"
+  cidr_block       = "10.0.11.0/24"
+  tags = {
+        Name = "private_subnet_a"
+  }
+
+}
+resource "aws_subnet" "private_subnet_b" {
+    vpc_id = aws_vpc.my_vpc.id
+  availability_zone = "us-west-2a"
+  cidr_block       = "10.0.12.0/24"
+  tags = {
+        Name = "private_subnet_b"
+  }
+}
+resource "aws_subnet" "private_subnet_c" {
+    vpc_id = aws_vpc.my_vpc.id
+  availability_zone = "us-west-2a"
+  cidr_block       = "10.0.13.0/24"
+  tags = {
+        Name = "private_subnet_c"
+  }
+
+}
+
+
+#    --- Create a private route table  ---
+
 resource "aws_route_table" "tasktest_private_route_table" {
-  vpc_id = aws_vpc.tf_vpc.id
+  vpc_id = aws_vpc.my_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gway.id
   }
+  tags = {
+        Name = "private_route_table"
+  }
+
+}
+
+
+
+#   ---Associate subnets to Private Route Table---
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id = aws_subnet.private_subnet_a.id
+  route_table_id = aws_route_table.tasktest_private_route_table.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id = aws_subnet.private_subnet_b.id
+  route_table_id = aws_route_table.tasktest_private_route_table.id
+}
+
+resource "aws_route_table_association" "private_c" {
+  subnet_id = aws_subnet.private_subnet_c.id
+  route_table_id = aws_route_table.tasktest_private_route_table.id
+}
+
+
+#   --- Create NAT gateway ---
+
+resource "aws_nat_gateway" "nat_gway" {
+  connectivity_type = "public"
+  subnet_id         = aws_subnet.public_a.id
+  allocation_id = aws_eip.nat_gateway_eip.id
 }
