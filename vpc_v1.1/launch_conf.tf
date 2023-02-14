87abb498d49a0686d87d2ec5fe0e5ecd80fe8c2e
oksanaivashko <oksivashko13@gmail.com>
2023-02-14 12:13:30 -0600

    testing sg

--- vpc_v1.1/launch_conf.tf
+++ vpc_v1.1/launch_conf.tf
@@ -2,6 +2,6 @@ resource "aws_launch_configuration" "linux" {
   name = var.task-launch-config
   image_id = data.aws_ami.amazon_linux_2.id
   instance_type = var.instance_type
-  security_groups = [ aws_security_group.main_sg1.id ]
+  #security_groups = [ aws_security_group.main_sg1.id ]
   key_name = var.windows-key
 }
