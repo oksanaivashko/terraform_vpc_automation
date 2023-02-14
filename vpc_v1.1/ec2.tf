78a844b1babb62a23a348164386dc504abbf5edb
oksanaivashko <oksivashko13@gmail.com>
2023-02-14 12:15:43 -0600

    modified sg

--- vpc_v1.1/ec2.tf
+++ vpc_v1.1/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "first_ec2" {
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = var.instance_type
   subnet_id = aws_subnet.public_subnet_a.id
-  vpc_security_group_ids = ["aws_security_group.main_sg.id"]
+  vpc_security_group_ids = ["aws_security_group.main_sg1.id"]
   tags = {
     Name     = var.tags_ec2
   }
